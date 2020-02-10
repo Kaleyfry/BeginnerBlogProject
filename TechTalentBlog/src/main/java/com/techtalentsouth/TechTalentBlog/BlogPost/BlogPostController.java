ba30bda76fb5fb534324dd35d375b61645c33022
Kaleyfry <59694186+Kaleyfry@users.noreply.github.com>
2020-02-10 11:40:10 -0500

    Add files via upload
    Commit 1

---
+++ TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -0,0 +1,53 @@
+package com.techtalentsouth.TechTalentBlog.BlogPost;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BlogPostController {
+	
+	@Autowired
+	private BlogPostRepository blogPostRepository;
+	private static List<BlogPost> posts = new ArrayList<>();
+	
+	@GetMapping(value="/")
+	public String index(BlogPost blogPost, Model model) {
+		model.addAttribute("posts",posts);
+		return "blogpost/index";
+	    }
+	
+	private BlogPost blogPost;
+	
+	@GetMapping(value="/blog_posts/new")
+		public String newBlog (BlogPost blogpost) {
+		return "blogpost/new";
+	}
+
+	
+	 @PostMapping(value = "/blog_posts/new")
+	    public String create(BlogPost blogPost, Model model) {
+		blogPostRepository.save(blogPost);
+		posts.add(blogPost);
+		model.addAttribute("title", blogPost.getTitle());
+		model.addAttribute("author", blogPost.getAuthor());
+		model.addAttribute("blogEntry", blogPost.getBlogEntry());
+		return "blogpost/results";
+	    }
+	 
+//	 @PostMapping(value = "/")
+//	    public String addNewBlogPost(BlogPost blogPost, Model model) {
+//		blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+//		model.addAttribute("title", blogPost.getTitle());
+//		model.addAttribute("author", blogPost.getAuthor());
+//		model.addAttribute("blogEntry", blogPost.getBlogEntry());
+//		return "blogPost/result";
+//	    }
+
+
+}
